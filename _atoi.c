9001eb34b9d9c59aec5c4fe467ef547f9bde8b01
Edwin mutua <edwinmutua54@gmail.com>
2023-10-17 09:42:11 -0700

    simple shell

--- _atoi.c
+++ _atoi.c
@@ -1,53 +1,51 @@
 #include "shell.h"
 
 /**
- * is_interactive - Check if shell is in interactive mode.
- * @info: Pointer to struct info_t.
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
  *
- * Return: 1 if in interactive mode, 0 otherwise.
+ * Return: 1 if interactive mode, 0 otherwise
  */
-int is_interactive(info_t *info)
+int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
- * is_delimiter - Check if a character is a delimiter.
- * @c: The character to check.
- * @delim: The delimiter string.
- *
- * Return: 1 if it's a delimiter, 0 if not.
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the delimeter string
+ * Return: 1 if true, 0 if false
  */
-int is_delimiter(char c, char *delim)
+int is_delim(char c, char *delim)
 {
 	while (*delim)
-	{
 		if (*delim++ == c)
-		{
 			return (1);
-		}
-	}
 	return (0);
 }
 
 /**
- * is_alpha - Check if a character is alphabetic.
- * @c: The character to check.
- *
- * Return: 1 if it's alphabetic, 0 otherwise.
+ * _isalpha - checks for alphabetic character
+ * @c: The character to input
+ * Return: 1 if c is alphabetic, 0 otherwise
  */
-int is_alpha(int c)
+
+int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 
 /**
- * string_to_int - Convert a string to an integer.
- * @s: The string to be converted.
- *
- * Return: 0 if no numbers in string, the converted number otherwise.
+ * _atoi - converts a string to an integer
+ * @s: the string to be converted
+ * Return: 0 if no numbers in string, converted number otherwise
  */
-int string_to_int(char *s)
+
+int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
@@ -55,9 +53,7 @@ int string_to_int(char *s)
 	for (i = 0; s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
-		{
 			sign *= -1;
-		}
 
 		if (s[i] >= '0' && s[i] <= '9')
 		{
@@ -66,19 +62,13 @@ int string_to_int(char *s)
 			result += (s[i] - '0');
 		}
 		else if (flag == 1)
-		{
 			flag = 2;
-		}
 	}
 
 	if (sign == -1)
-	{
 		output = -result;
-	}
 	else
-	{
 		output = result;
-	}
 
 	return (output);
 }
