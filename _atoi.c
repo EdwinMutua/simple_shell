cf9e6ce5d8add2a0dddde5a28c181f34bed3a0d4
Edwin mutua <edwinmutua54@gmail.com>
2023-10-18 08:36:13 -0700

    simple shell

--- _atoi.c
+++ _atoi.c
@@ -1,53 +1,42 @@
-#include "shell.h"
-
-int is_interactive(info_t *info)
-{
-    return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
-
-int is_delim(char c, char *delim)
-{
-    while (*delim)
-    {
-        if (*delim++ == c)
-            return (1);
-    }
-    return (0);
-}
-
-int _is_alpha(int c)
-{
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return (1);
-    else
-        return (0);
-}
-
+/**
+ * _atoi - Convert a string to an integer
+ * @s: The input string
+ *
+ * Return: The converted integer
+ */
 int _atoi(char *s)
 {
-    int i, sign = 1, flag = 0, output;
-    unsigned int result = 0;
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-    for (i = 0; s[i] != '\0' && flag != 2; i++)
-    {
-        if (s[i] == '-')
-            sign *= -1;
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
 
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            flag = 1;
-            result *= 10;
-            result += (s[i] - '0');
-        }
-        else if (flag == 1)
-            flag = 2;
-    }
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+		{
+			flag = 2;
+		}
+	}
 
-    if (sign == -1)
-        output = -result;
-    else
-        output = result;
+	if (sign == -1)
+	{
+		output = -result;
+	}
+	else
+	{
+		output = result;
+	}
 
-    return (output);
+	return (output);
 }
 
