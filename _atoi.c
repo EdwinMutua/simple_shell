cac024315820e8088d82946eca91304b83700cd8
erickmurigi <ericmurigi04@gmail.com>
2023-10-19 09:52:46 -0700

    0

--- _atoi.c
+++ _atoi.c
@@ -1,53 +1,66 @@
 #include "shell.h"
+/**
+ * is_interactive - returns 1 if shell is interactive
+ * @info: struct
+ * Return: Return 1 if interactive otherwise 0
+ */
 
 int is_interactive(info_t *info)
 {
-    return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
-
+/**
+ * is_delim - checks if character is a delimeter
+ * @c: the char to check
+ * @delim: the string delimeter
+ * Return: if true,return 1, 0 if false
+ */
 int is_delim(char c, char *delim)
 {
-    while (*delim)
-    {
-        if (*delim++ == c)
-            return (1);
-    }
-    return (0);
+	while (*delim)
+	{
+		if (*delim++ == c)
+			return (1);
+	}
+	return (0);
 }
-
+/**
+ *_is_alpha - check alphabetic characters
+ *@c: character to input
+ *Return: Return 1 if  alphabet, 0 otherwise
+ */
 int _is_alpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return (1);
-    else
-        return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
-
+/**
+ *_atoi - converts string to integer
+ *@s: the string to convert
+ *Return: Return 0 if no nums in string
+ */
 int _atoi(char *s)
 {
-    int i, sign = 1, flag = 0, output;
-    unsigned int result = 0;
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-    for (i = 0; s[i] != '\0' && flag != 2; i++)
-    {
-        if (s[i] == '-')
-            sign *= -1;
-
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            flag = 1;
-            result *= 10;
-            result += (s[i] - '0');
-        }
-        else if (flag == 1)
-            flag = 2;
-    }
-
-    if (sign == -1)
-        output = -result;
-    else
-        output = result;
-
-    return (output);
+	for (i = 0; s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+			else if (flag == 1)
+				flag = 2;
+		}
+		if (sign == -1)
+			output = -result;
+		else
+			output = result;
+		return (output);
 }
-
--- history.c
+++ history.c
@@ -63,72 +63,67 @@ int write_history(info_t *info)
  */
 int read_history(info_t *info)
 {
-    char *filename = get_history_file(info);
-    if (!filename)
-        return (0);
+	char *filename = get_history_file(info);
+	ssize_t fd;
+	struct stat st;
+	off_t fsize;
+	char *buf;
+	ssize_t rdlen;
+	int i, last = 0, linecount = 0;
+	
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	free(filename);
+	if (fd == -1)
+		return (0);
+    	if (fstat(fd, &st) == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	fsize = st.st_size;
+	if (fsize < 2)
+	{
+		close(fd);
+		return (0);
+	}
+	buf = malloc(sizeof(char) * (fsize + 1));
 
-    ssize_t fd = open(filename, O_RDONLY);
-    free(filename);
-
-    if (fd == -1)
-        return (0);
-
-    struct stat st;
-    if (fstat(fd, &st) == -1)
-    {
-        close(fd);
-        return (0);
-    }
-
-    off_t fsize = st.st_size;
-    if (fsize < 2)
-    {
-        close(fd);
-        return (0);
-    }
-
-    char *buf = malloc(sizeof(char) * (fsize + 1));
-    if (!buf)
-    {
-        close(fd);
-        return (0);
-    }
-
-    ssize_t rdlen = read(fd, buf, fsize);
-    buf[fsize] = '\0';
-    close(fd);
-
-    if (rdlen <= 0)
-    {
-        free(buf);
-        return (0);
-    }
-
-    int i = 0;
-    int last = 0;
-    int linecount = 0;
-
-    for (i = 0; i < fsize; i++)
-    {
-        if (buf[i] == '\n')
-        {
-            buf[i] = '\0';
-            build_history_list(info, buf + last, linecount++);
-            last = i + 1;
-        }
-    }
-
-    if (last != i)
-        build_history_list(info, buf + last, linecount++);
-
-    free(buf);
-    info->histcount = linecount;
-
-    while (info->histcount-- >= HIST_MAX)
-        delete_node_at_index(&(info->history), 0);
-
-    renumber_history(info);
-    return (info->histcount);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+	
+	rdlen = read(fd, buf, fsize);
+	buf[fsize] = '\0';
+	close(fd);
+	
+	if (rdlen <= 0)
+	{
+		free(buf);
+		return (0);
+	}
+	for (i = 0; buf[i] != '\0'; i++)
+	{
+		if (buf[i] == '\n')
+		{
+			build_history_list(info, buf + last, linecount++);
+			last = i + 1;
+		}
+	}
+	
+	if (last != i)
+		build_history_list(info, buf + last, linecount++);
+	
+	free(buf);
+	info->histcount = linecount;
+	
+	while (info->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(info->history), 0);
+	renumber_history(info);
+	return (info->histcount);
 }
 
 /**
@@ -141,17 +136,16 @@ int read_history(info_t *info)
  */
 int build_history_list(info_t *info, char *buf, int linecount)
 {
-    list_t *node = NULL;
-
-    if (info->history)
-        node = info->history;
-
-    add_node_end(&node, buf, linecount);
-
-    if (!info->history)
+	list_t *node = NULL;
+	
+	if (info->history)
+		node = info->history;
+	
+	add_node_end(&node, buf, linecount);
+	if (!info->history)
         info->history = node;
-
-    return (0);
+	
+	return (0);
 }
 
 /**
@@ -162,14 +156,14 @@ int build_history_list(info_t *info, char *buf, int linecount)
  */
 int renumber_history(info_t *info)
 {
-    list_t *node = info->history;
-    int i = 0;
-
-    while (node)
-    {
-        node->num = i++;
-        node = node->next;
-    }
-
-    return (info->histcount = i);
+	list_t *node = info->history;
+	int i = 0;
+	
+	while (node)
+	{
+		node->num = i++;
+		node = node->next;
+	}
+	
+	return (info->histcount = i);
 }
